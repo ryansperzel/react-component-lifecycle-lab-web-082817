87b9f50dacf3babcf26e4dc3bf119e5344f4e63c
Ryan Sperzel <ryansperzel@gmail.com>
2017-11-02 18:23:39 -0400

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -11,8 +11,27 @@ class TweetWall extends React.Component {
   }
 
   // TODO: componentWillMount()
+
+  componentWillMount() {
+    this.setState({
+      tweets: this.props.newTweets
+    })
+  }
   // TODO: shouldComponentUpdate()
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if(nextProps.newTweets.length > 0) {
+      return true
+    }
+    return false
+  }
   // TODO: componentWillReceiveProps()
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      tweets: [...nextProps.newTweets, ...this.state.tweets]
+    })
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
